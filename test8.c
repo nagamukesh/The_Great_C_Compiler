30d2a17554f27ab995f039238c74a48723db44d8
GARAPATI TEJA SRI <148844350+Arnella16@users.noreply.github.com>
2024-10-20 20:06:16 +0530

    Update test8.c

--- test8.c
+++ test8.c
@@ -1,12 +1,22 @@
+//ERROR FREE - This test case includes nested loops
+
 #include<stdio.h>
 
 int main()
 {
-	int num = 3;
-	
-	for(int i = 0; i<num; i++)
-	{
-		for(int j = 0; j < num; j++)
-			printf("Hello");
-	}
+        int num = 3;
+
+        for(int i = 0; i<num; i++)
+        {
+                for(int j = 0; j < num; j++)
+                        printf("Hello");
+        }
+        do{
+                int flag=2;
+        }while(num>0);
+        int *a;
+        switch(num){
+                case 3: num++;
+                default: break;
+        }
 }
