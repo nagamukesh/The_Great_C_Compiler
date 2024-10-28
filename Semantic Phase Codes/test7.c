bf947baeee6a2e61ced555f627d7be2d3fdee080
GARAPATI TEJA SRI <148844350+Arnella16@users.noreply.github.com>
2024-10-28 21:51:55 +0530

    Update test7.c

--- Semantic Phase Codes/test7.c
+++ Semantic Phase Codes/test7.c
@@ -3,12 +3,12 @@
 static int count = 5; /* global variable */
 
 /* function definition */
-void func(int gay)
+void func(int k)
 {
 
     static int i = 5; /* local static variable */
     i++;
-    gay++;
+    k++;
     // printf("i is %d and count is %d\n", i, count);
 }
 
